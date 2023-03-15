a5106612a81add7bd49039c56a7b2b499a1a54a9
felixNyalenda <felixasejunior@gmail.com>
2023-03-15 18:49:26 +0300

    Challenge 3 and README.md update

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-WK1-Code-Challenge
+Title: WK1-Code-Challenge
+Description: This is an assignment in the form of code challenges testing on the topic of Javascripts's if statements.
+There are 3 code challenges that I'm required to solve and submit for review.
+The code challenges can be found on the **index.js** file each with a labelled tag for easier identification.
+
+Project Setup: The project contains a **index.html**, **index.js**, and **README.md**
+
+Challenge 1: Student Grade Generator
+gradeGenerator(grade) is function that takes in grade(typeof 'number') as a parameter and returns a student's grade depending with the input.
+A > 79, B - 60 to 79, C - 59 to 49, D - 40 to 49, E - less 40.
+
+Challenge 2: Speed Detector
+speedDetector() is function that takes in speed(typeof 'number') as a parameter and gauges the driver's speed, for every 5kph above the speed limit of 70kph, a driver is given a demerit point of which more than 12 points, the driver's licence would be suspended.
+
+Challenge 3: Net Salary Calculator
+This is a program that takes in an individual's basic pay and other benefits earned and calcutates the individuals net salary after statutory deductions.
+
+Project Author: Felix Nyalenda Olali
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Challenge 1: Student Grade Generator
-
 function gradeGenerator(grade) {
    if (grade <= 100 && grade >= 0) {
       if (grade > 79) {
@@ -18,11 +17,9 @@ function gradeGenerator(grade) {
       console.log("Input number between 0-100");
    }
 }
-
 // gradeGenerator(23);
 
 // Challenge 2: Speed Detector
-
 function speedDetector(speed) {
    let points = 0;
    if (speed > 70) {
@@ -37,5 +34,125 @@ function speedDetector(speed) {
       return "Ok";
    }
 }
+speedDetector(164);
 
-console.log(speedDetector(164));
+// Challenge 3: Net Salary Calculator
+function salaryCalculator(basicPay, benefits) {
+   const grossPay = basicPay + benefits;
+   const nssfOldRates = 200;
+   const pension = 0.06 * grossPay;
+   //    Calculates the taxable Income
+   const taxableIncome = grossPay - (nssfOldRates + pension);
+
+   //    tax relief calculator
+   const taxReliefCalculator = (taxableIncome) => {
+      if (taxableIncome < 24000) {
+         return (taxRelief = 0);
+      } else {
+         return (taxRelief = 2400);
+      }
+   };
+
+   //    Payroll parameters
+   const taxReliefConst = taxReliefCalculator(taxableIncome);
+   const taxPayableConst = taxCalculator(taxableIncome);
+   const nhifConst = nhifCalculator(grossPay);
+   const netPay = grossPay - nssfOldRates - pension - (taxPayableConst - taxReliefConst) - nhifConst;
+
+   console.log(`Gross pay: ${grossPay}`);
+   console.log(`NSSF: ${nssfOldRates}`);
+   console.log(`Pension: ${pension}`);
+   //    taxCalculator(grossPay, nssfOldRates, pension);
+
+   return `Gross Salary: Ksh ${grossPay}, NSSF: Ksh ${nssfOldRates}, Pension: Ksh ${pension}, Tax payable: Ksh ${taxPayableConst}, Tax Relief: Ksh ${taxReliefConst}, NHIF: Ksh ${nhifConst}, Net Pay: Ksh ${netPay.toLocaleString()}`;
+}
+
+// Gross Salary calculator
+function taxCalculator(taxableIncome) {
+   if (taxableIncome >= 24000) {
+      let taxBracket1;
+      let taxBracket2;
+      let taxBracket3;
+      let taxPayable;
+      if (taxableIncome < 24001) {
+         taxBracket1 = 0.1 * taxableIncome;
+         taxPayable = Math.round(taxBracket1);
+         //  console.log(`tax payable: ${taxPayable}`);
+         return taxPayable;
+      } else if (taxableIncome > 24000 && taxableIncome <= 32333) {
+         taxBracket1 = 0.1 * 24000;
+         taxBracket2 = 0.25 * (taxableIncome - 24000);
+         taxPayable = Math.round(taxBracket1 + taxBracket2);
+         //  console.log(`tax payable: ${taxPayable}`);
+         return taxPayable;
+      } else if (taxableIncome > 32333) {
+         taxBracket1 = 0.1 * 24000;
+         taxBracket2 = 0.25 * (32333 - 24000);
+         taxBracket3 = 0.3 * (taxableIncome - 32333);
+         taxPayable = Math.round(taxBracket1 + taxBracket2 + taxBracket3);
+         //  console.log(`tax payable: ${taxPayable}`);
+         return taxPayable;
+      }
+   }
+}
+
+// console.log(taxCalculator(65450, 200, 3927));
+
+// NHIF Calculator
+function nhifCalculator(grossPay) {
+   let nhifRate;
+   if (grossPay <= 5999) {
+      nhifRate = 150;
+      return nhifRate;
+   } else if (grossPay >= 6000 && grossPay <= 7999) {
+      nhifRate = 300;
+      return nhifRate;
+   } else if (grossPay >= 8000 && grossPay <= 11999) {
+      nhifRate = 400;
+      return nhifRate;
+   } else if (grossPay >= 12000 && grossPay <= 14999) {
+      nhifRate = 500;
+      return nhifRate;
+   } else if (grossPay >= 15000 && grossPay <= 19999) {
+      nhifRate = 600;
+      return nhifRate;
+   } else if (grossPay >= 20000 && grossPay <= 24999) {
+      nhifRate = 750;
+      return nhifRate;
+   } else if (grossPay >= 25000 && grossPay <= 29999) {
+      nhifRate = 850;
+      return nhifRate;
+   } else if (grossPay >= 30000 && grossPay <= 34999) {
+      nhifRate = 900;
+      return nhifRate;
+   } else if (grossPay >= 35000 && grossPay <= 34999) {
+      nhifRate = 950;
+      return nhifRate;
+   } else if (grossPay >= 40000 && grossPay <= 44999) {
+      nhifRate = 1000;
+      return nhifRate;
+   } else if (grossPay >= 45000 && grossPay <= 49999) {
+      nhifRate = 1100;
+      return nhifRate;
+   } else if (grossPay >= 50000 && grossPay <= 59999) {
+      nhifRate = 1200;
+      return nhifRate;
+   } else if (grossPay >= 60000 && grossPay <= 69999) {
+      nhifRate = 1300;
+      return nhifRate;
+   } else if (grossPay >= 70000 && grossPay <= 79999) {
+      nhifRate = 1400;
+      return nhifRate;
+   } else if (grossPay >= 80000 && grossPay <= 89999) {
+      nhifRate = 1500;
+      return nhifRate;
+   } else if (grossPay >= 90000 && grossPay <= 99999) {
+      nhifRate = 1600;
+      return nhifRate;
+   } else if (grossPay >= 100000) {
+      nhifRate = 1700;
+      return nhifRate;
+   }
+}
+
+console.log(salaryCalculator(95000, 5000));
